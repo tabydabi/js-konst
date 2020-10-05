3cdbd9ba9c13e2321c3df0c364f109774dc54392
Kirill Borun <kbroun@yandex.by>
2020-10-05 23:50:28 +0300

    first commit

---
+++ src/model.js
@@ -0,0 +1,6 @@
+export const model = [
+    {type: 'title', value: 'Hello World from JS'},
+    {type: 'text', value: 'here we go with smt text'},
+    {type: 'columns', value: ['11111','22222','33333','44444']},
+    {type : 'image', value: './assets/image.png'}
+]
